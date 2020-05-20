64c7632dabf28ffe885d8be7727301709261e75d
Uriah Harston <uharston@gmail.com>
2020-05-20 16:04:05 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+  attr_accessor :cost
+  attr_reader :meal, :customer, :waiter
+
+  @@all = []
+
+  def initialize(meal, customer, waiter)
+    @meal = meal
+    @customer = customer
+    @waiter = waiter
+    @cost = 0
+    @@all << self
+  end
+
+   def self.all
+     @@all
+   end
 
 end
