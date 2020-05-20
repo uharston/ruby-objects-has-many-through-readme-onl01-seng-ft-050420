00d1db1078fcb4f34ea2b0b9f22e33a731bafd4c
Uriah Harston <uharston@gmail.com>
2020-05-20 16:16:25 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -29,8 +29,9 @@ class Waiter
  def best_tipper
    best_tipped_meal =  meals.map {|meal| meal.tip.sort.last}
    best_tipped_meal
+   binding.pry
  end
-binding.pry
+
  # def best_tipper
  #   best_tipped_meal = meals.max do |meal_a, meal_b|
  #     meal_a.tip <=> meal_b.tip
